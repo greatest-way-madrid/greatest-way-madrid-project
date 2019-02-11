c390efde9b9939f0f1fb37c7c21cf17291dc2ef3
Javier Requejo Melchor <javierrequejomelchor@gmail.com>
2019-02-11 15:56:46 +0100

    fit - Card's style improved.

--- helpers/route.helpers.js
+++ helpers/route.helpers.js
@@ -1,11 +1,11 @@
 module.exports = (hbs) => {
-  hbs.registerHelper('additional', (book) => {
-      if (book.rating >= 8) {
-          return 'success';
-      } else if (book.rating >= 5) {
-          return 'warning';
-      } else {
-          return 'danger';
+  hbs.registerHelper('get-img-src', (trip) => {
+      switch(trip.kind) {
+          case 'driving': return '/images/car.png'; break;
+          case 'transit': return '/images/train.png'; break;
+          case 'bicycling': return '/images/bicycle.png'; break;
+          case 'walking': return '/images/trekking.png'; break;
+          case 'uber': return '/images/car.png'; break;
       }
   });
 }
